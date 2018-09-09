decff013934ecb9ffca10796d7c8267fd711116f
fg222ak <fg222ak@student.lnu.se>
2018-09-09 11:55:11 +0200

    Add datetime with appropriate formatting

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,9 +4,12 @@ class DateTimeView {
 
 
 	public function show() {
+		$day = date('l');
+		$dayOfMonth = date('jS');
+		$month = date('F');
+		$year = date('Y');
+		$currentTime = date('h:i:s');
 
-		$timeString = 'TODO, Write servertime here...';
-
-		return '<p>' . $timeString . '</p>';
+		return "<p> {$day}, the {$dayOfMonth} of {$month} {$year}. The time is {$currentTime} </p>";
 	}
 }
