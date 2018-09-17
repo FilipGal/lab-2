f372dc0cdf98c171fe7ad647fed0330c27ce8a4b
fg222ak <fg222ak@student.lnu.se>
2018-09-17 15:54:16 +0200

    Move rendering to a controller

--- controller/MainController.php
+++ controller/MainController.php
@@ -1,5 +1,25 @@
 <?php
+require_once 'view/LoginView.php';
+require_once 'view/LayoutView.php';
+require_once 'view/RegisterView.php';
+
 class MainController
 {
+
+    public function __construct()
+    {
+        $this->loginView = new LoginView();
+        $this->layoutView = new LayoutView();
+        $this->registerView = new RegisterView();
+    }
+
+    public function render()
+    {
+        $this->layoutView->renderLayoutView(
+            $this->loginView->isLoggedIn(),
+            $this->loginView,
+            $this->registerView
+        );
+    }
 
 }
--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
+require_once 'controller/MainController.php';
 
 if (!isset($_SESSION)) {
     session_start();
 }
 
-//INCLUDE THE FILES NEEDED...
-require_once 'view/LoginView.php';
-require_once 'view/DateTimeView.php';
-require_once 'view/LayoutView.php';
-require_once 'view/RegisterView.php';
-
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-// var_dump($_SESSION);
+$c = new MainController();
 
-//CREATE OBJECTS OF THE VIEWS
-$loginView = new LoginView();
-$layoutView = new LayoutView();
-$registerView = new RegisterView();
-
-$layoutView->renderLayoutView($loginView->isLoggedIn(), $loginView, $registerView);
+$c->render();
